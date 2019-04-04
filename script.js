226dd7237836b6663e49770b74b18ed87f494142
Moorad <34781684+mooradal@users.noreply.github.com>
2019-04-05 00:21:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('Working');
+
+var close = document.getElementById('close');
+var object = document.getElementById('game');
+var game = document.getElementsByClassName('game')[0];
+
+
+close.addEventListener('click', () => {
+    game.style.display = 'none';
+});
+
+function load(element) {
+    object.setAttribute('data', 'games/'+element.dataset.file);
+    game.style.display = 'block';
+}
